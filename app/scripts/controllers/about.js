62b03533044db324f0db2512154a544bbd172d57
Gustav Danielsson <gdanielsson@me.com>
2016-09-04 22:45:47 -0400

    add angular template files, karma test files, grunt file.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gd8githubioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the gd8githubioApp
+ */
+angular.module('gd8githubioApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name gd8githubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the gd8githubioApp
+ */
+angular.module('gd8githubioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
